ade227d4355eee47c9346b61f3a0f56f595f905b
Aaryanajith <aaryanajith45@gmail.com>
2023-01-17 19:57:11 +0530

    Modified templates to display posts from database.

--- my_blogsite/views.py
+++ my_blogsite/views.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, '/home/interstellar/Django/my_blogsite/template/my_blogsite/post_list.html', {'posts': posts})
+    return render(request, 'my_blogsite/post_list.html', {'posts': posts})
     
